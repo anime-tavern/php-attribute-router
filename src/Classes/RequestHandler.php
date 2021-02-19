a8f8a13e810bb43ffc0846887c10c670fac18789
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-02-19 11:22:02 -0600

    Create RequestHandler.php

---
+++ src/Classes/RequestHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+	require_once __DIR__ . "/ViewSettings.php";
+	require_once __DIR__ . "/Router.php";
+
+	class RequestHandler{
+
+		/**
+		* Processes and routes a Swoole request
+		* @param ViewSettings $viewSettings
+		* @param Router $router
+		*/
+		public static function process(string $requestPath, Router $router, StaticFileHandler $staticFileHandler){
+			$serverData = $request->server;
+			$clientIP = $_SERVER['REMOTE_ADDR'];
+			$requestType = $_SERVER['REQUEST_METHOD'];
+
+			if ($requestType === "GET"){
+
+				// Check for a static file
+				if ($staticFileHandler->doesStaticFileExist($requestPath)){
+					$mimeType = $staticFileHandler->getStaticFileMime($requestPath);
+					if ($mimeType === null){
+						$mimeType = "text/plain";
+					}
+
+					/**
+					* Set the cache-control header if there is a cache config for
+					* the given mime type
+					*/
+					$cacheTime = $staticFileHandler->getCacheTimeForMime($mimeType);
+					if ($cacheTime !== null){
+						$response->header("cache-control", sprintf("max-age=%d", $cacheTime));
+					}
+
+					header("content-type: $mimeType");
+					return $staticFileHandler->getStaticFileContents($requestPath);
+				}
+			}
+
+			return $router->route($requestType, $requestPath);
+		}
+
+	}
