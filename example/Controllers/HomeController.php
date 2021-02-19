f9c6b4015cb0a33f5a040dcdff98f7c4c7dc06ca
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-02-19 11:21:41 -0600

    Create HomeController.php

---
+++ example/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+	require_once __DIR__ . "/../../src/Classes/ViewSettings.php";
+	require_once __DIR__ . "/../../src/Classes/RenderEngine/Renderer.php";
+
+	class HomeController{
+
+		private ?ViewSettings $viewSettings;
+
+		/**
+		* @param ViewSettings $viewSettings
+		*/
+		public function __construct(ViewSettings $viewSettings){
+			$this->viewSettings = $viewSettings;
+		}
+
+		/**
+		* @param Swoole\Http\Request $request
+		* @param Swoole\Http\Response $response
+		* @return string
+		*/
+		#[Route("GET", "/")]
+		public function homePage(){
+			header("content-type: text/html");
+
+			// Get the view file
+			$renderer = new RenderEngine\Renderer($this->viewSettings->getViewFilePath("home.php"), $this->viewSettings);
+			return $renderer->getRenderedViewFile();
+		}
+
+	}
