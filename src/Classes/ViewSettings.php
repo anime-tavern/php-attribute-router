2da1bd2879741af8d15732cffc679c7e473beeb5
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-02-19 11:22:10 -0600

    Create ViewSettings.php

---
+++ src/Classes/ViewSettings.php
@@ -0,0 +1,17 @@
+<?php
+	class ViewSettings{
+		public $viewsFolder = "";
+		public $layoutsFolder = "";
+
+		public function setViewsFolder(string $directoryPath){
+			$this->viewsFolder = $directoryPath;
+		}
+
+		public function setLayoutsFolder(string $directoryPath){
+			$this->layoutsFolder = $directoryPath;
+		}
+
+		public function getViewFilePath(string $fileName){
+			return sprintf("%s/%s", $this->viewsFolder, $fileName);
+		}
+	}
